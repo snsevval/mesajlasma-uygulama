8f96546ce8ef7a32514e1419f2ce23914bef7c3c
snsevval <148317762+snsevval@users.noreply.github.com>
2024-05-18 00:13:04 +0300

    Proje tamamlandı.

---
+++ README.md
@@ -0,0 +1,4 @@
+Mobil Programlama Dersi için Android Studio'da Java'da yazdığım ufak çaplı mesajlasma uygulamasi.
+
+Uygulama video linki:
+https://youtu.be/Fc9MN0rB64k
---
+++ app/src/main/java/com/example/mesajlasmauygulamasi/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.mesajlasmauygulamasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.mesajlasmauygulamasi.utils.FirebaseUtil;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(FirebaseUtil.isLoggedIn()){
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }else{
+                    startActivity(new Intent(SplashActivity.this,LoginPhoneNumberActivity.class));
+                }
+                finish();
+            }
+        }, 1500);
+
+    }
+}
